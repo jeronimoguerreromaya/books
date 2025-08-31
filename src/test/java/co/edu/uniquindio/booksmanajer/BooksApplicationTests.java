002d0f8677586fe33e1a7d6737913431e71fd8b7
jero <omineroj@gmail.com>
2025-08-31 09:34:33 -0500

    primer commmit

---
+++ src/test/java/co/edu/uniquindio/booksmanajer/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.booksmanajer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
